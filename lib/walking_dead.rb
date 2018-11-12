3d14fb7961fd654fba114680710dac3741e914b2
Seiei Miyagi <hanachin@gmail.com>
2018-11-12 15:54:04 +0900

    bundle gem walking_dead

---
+++ lib/walking_dead.rb
@@ -0,0 +1,6 @@
+require "walking_dead/version"
+
+module WalkingDead
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/walking_dead/version.rb
@@ -0,0 +1,3 @@
+module WalkingDead
+  VERSION = "0.1.0"
+end
